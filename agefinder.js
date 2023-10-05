c168424be6599fbc248b71b843d2142c6731f0cf
Rangarajpalani <rangapalani321@gmail.com>
2023-10-04 20:00:37 -0700

    modified something

--- agefinder.js
+++ agefinder.js
@@ -1,9 +1,9 @@
-let dob = "20-07-1982";
+let dob = "20-02-2001";
 let s = dob.split('-').map((str) => { return parseInt(str); })
 let year = s[2];
-let date =new date();
-let currentyear = date.getfullyear();
+const totaydate = new Date();
+let currentyear = totaydate.getFullYear();
 const age = (year, currentyear) => {
     return currentage = currentyear - year;
 }
-console.log(age(year, currentyear))
+console.log(age(year, currentyear));
