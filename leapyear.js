ee04546f52d715d2178c7e3697896be672f7d4eb
Ravikumar  K <kkravikumar@gmail.com>
2023-10-06 09:55:59 -0400

    leap year logic added

--- leapyear.js
+++ leapyear.js
@@ -1,6 +1,6 @@
 let year = 2023;
 const leapyear = (year) => {
-    if (year % 4 === 0 && year % 100 !== 0) {
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ) {
         return true;
     }
     return false;
